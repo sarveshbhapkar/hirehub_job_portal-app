1829fc550771ca01a52bb194732d3b751b0e08a3
sarvesh bhapkar <sarveshbhapkar100@gmail.com>
2024-08-31 20:14:58 +0530

    connecting supabase with clerk

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -3,7 +3,22 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+const supabaseClient = async (supaAccessToken) =>{
+    const supabase = createClient(supabaseUrl,supabaseKey,{
+        global:{
+
+            headers:{
+                Authorization:`Bearer ${supaAccessToken}`,
+            },
+        }
+    });
+
+    return supabase;
+}
+
+
 
 export default supabase
         
