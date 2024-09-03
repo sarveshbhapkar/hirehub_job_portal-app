08596fb67a72d346f3eeffa4c7e4aa975a8a17e6
sarvesh bhapkar <sarveshbhapkar100@gmail.com>
2024-09-03 23:23:25 +0530

    search and filterjobs functionality

---
+++ src/api/apiCompanies.js
@@ -0,0 +1,16 @@
+import supabaseClient from "@/utils/supabase";
+
+export async function getCompanies(token){
+
+    const supabase = await supabaseClient(token);
+
+    const {data,error} = await supabase.from("companies").select("*");
+
+    if(error){
+        console.error("error fetching companies:",error);
+        return null;
+    }
+    return data;
+    
+
+}
--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,46 +1,135 @@
-import { getJobs, getMyJobs } from '@/api/apijobs'
-import JobCard from '@/components/job-card'
-import useFetch from '@/hooks/use-fetch'
-import { useSession, useUser } from '@clerk/clerk-react'
-import React, { useEffect, useState } from 'react'
-import { BarLoader } from 'react-spinners'
+import { getCompanies } from "@/api/apicompanies";
+import { getJobs } from "@/api/apijobs";
+import JobCard from "@/components/job-card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import useFetch from "@/hooks/use-fetch";
+import { useUser } from "@clerk/clerk-react";
+import { State } from "country-state-city";
+import { useEffect, useState } from "react";
+import { BarLoader } from "react-spinners";
 
 const JobListing = () => {
-
-  const [searchQuery ,setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
   const [location, setLocation] = useState("");
   const [company_id, setCompany_id] = useState("");
-  const {isLoaded} = useUser();
+  const { isLoaded } = useUser();
 
+  const {
+    fn: fnJobs,
+    data: jobs,
+    loading: loadingJobs,
+  } = useFetch(getJobs, {
+    location,
+    company_id,
+    searchQuery,
+  });
 
-const {fn:fnJobs,
-        data:jobs,
-         loadingJobs,
-        } = useFetch(getMyJobs,{
-          location,
-          company_id,
-          searchQuery
-        });
+  const { fn: fnCompanies, data: companies } = useFetch(getCompanies);
 
-useEffect(()=>{
- if(isLoaded) fnJobs();
-},[isLoaded,location,company_id,searchQuery]);
+  useEffect(() => {
+    if (isLoaded) fnCompanies();
+  }, [isLoaded]);
 
-if (!isLoaded) {
-  return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
-}
+  useEffect(() => {
+    if (isLoaded) fnJobs();
+  }, [isLoaded, location, company_id, searchQuery]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    let formData = new FormData(e.target);
+
+    const query = formData.get("search-query");
+    if (query) setSearchQuery(query);
+  };
+
+  const clearFilters = () => {
+    setSearchQuery("");
+    setCompany_id("");
+    setLocation("");
+  };
+
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
 
   return (
     <div>
-      <h1 className='gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8'>Latest Jobs</h1>
+      <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">
+        Latest Jobs
+      </h1>
 
-      {/* add filters here  */}
+      {/* Add filters here */}
+      <form
+        onSubmit={handleSearch}
+        className="h-14 flex w-full gap-2 items-center mb-3"
+      >
+        <Input
+          type="text"
+          placeholder="Search Jobs by Title.."
+          name="search-query"
+          className="h-full flex-1 px-4 text-md"
+        />
+        <Button type="submit" className="h-full sm:w-28" variant="blue">
+          Search
+        </Button>
+      </form>
 
-    {loadingJobs && (<BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
-    
-    )}
+      <div className="flex flex-col sm:flex-row gap-2">
+        <Select value={location} onValueChange={(value) => setLocation(value)}>
+          <SelectTrigger>
+            <SelectValue placeholder="Filter by Location" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {State.getStatesOfCountry("IN").map(({ name }) => {
+                return (
+                  <SelectItem key={name} value={name}>
+                    {name}
+                  </SelectItem>
+                );
+              })}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
 
-{loadingJobs && (
+        <Select
+          value={company_id}
+          onValueChange={(value) => setCompany_id(value)}
+        >
+          <SelectTrigger>
+            <SelectValue placeholder="Filter by Company" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {companies?.map(({ name, id }) => {
+                return (
+                  <SelectItem key={name} value={id}>
+                    {name}
+                  </SelectItem>
+                );
+              })}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Button
+          onClick={clearFilters}
+          variant="destructive"
+          className="sm:w-1/2"
+        >
+          Clear Filters
+        </Button>
+      </div>
+
+      {loadingJobs && (
         <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
       )}
 
@@ -59,12 +148,10 @@ if (!isLoaded) {
           ) : (
             <div>No Jobs Found 😢</div>
           )}
-      </div>
-
-    )}
-
+        </div>
+      )}
     </div>
   );
 };
 
-export default JobListing
+export default JobListing;
