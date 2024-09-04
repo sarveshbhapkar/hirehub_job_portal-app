977deb6d1837cb5388c66e23118e0cbfcd30f615
sarvesh bhapkar <sarveshbhapkar100@gmail.com>
2024-09-05 00:37:02 +0530

    add new company

--- src/api/apiCompanies.js
+++ src/api/apiCompanies.js
@@ -12,5 +12,39 @@ export async function getCompanies(token){
     }
     return data;
     
-
 }
+
+export async function addNewCompany(token, _, companyData) {
+    const supabase = await supabaseClient(token);
+  
+    const random = Math.floor(Math.random() * 90000);
+    const fileName = `logo-${random}-${companyData.name}`;
+  
+    const { error: storageError } = await supabase.storage
+      .from("company-logo")
+      .upload(fileName, companyData.logo);
+  
+    if (storageError) {
+      console.error("Error Uploading Company Logo:", storageError);
+      return null;
+    }
+  
+    const logo_url = `${supabaseUrl}/storage/v1/object/public/company-logo/${fileName}`;
+  
+    const { data, error } = await supabase
+      .from("companies")
+      .insert([
+        {
+          name: companyData.name,
+          logo_url,
+        },
+      ])
+      .select();
+  
+    if (error) {
+      console.error("Error Submitting Company:", error);
+      return null;
+    }
+  
+    return data;
+  }
