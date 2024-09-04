63be7da01e17d615c4fc52501564b00c4b58dea0
sarvesh bhapkar <sarveshbhapkar100@gmail.com>
2024-09-04 18:35:55 +0530

    reacr hook from and zod

--- src/components/apply-job.jsx
+++ src/components/apply-job.jsx
@@ -12,73 +12,138 @@ import {
 import { Button } from './ui/button'
 import { RadioGroup, RadioGroupItem } from '@radix-ui/react-radio-group'
 import { Label } from '@radix-ui/react-label'
+import { z } from 'zod'
+import { Controller, useForm } from 'react-hook-form'
 
-const ApplyJobDrawer = ({user,job,applied=false,fetchJob}) => {
+
+const ApplyJobDrawer = ({ user, job, applied = false, fetchJob }) => {
+    const {
+      register,
+      handleSubmit,
+      control,
+      formState: { errors },
+      reset,
+    } = useForm({
+      resolver: zodResolver(schema),
+    });
+  
+    const {
+      loading: loadingApply,
+      error: errorApply,
+      fn: fnApply,
+    } = useFetch(applyToJob);
+  
+    const onSubmit = (data) => {
+      fnApply({
+        ...data,
+        job_id: job.id,
+        candidate_id: user.id,
+        name: user.fullName,
+        status: "applied",
+        resume: data.resume[0],
+      }).then(() => {
+        fetchJob();
+        reset();
+      });
+    };
+
+
   return (
-    <Drawer open={applied?false: undefined} >
-  <DrawerTrigger asChild>
-    <Button 
-    size='lg'
-    variant={job?.isOpen && !applied ? "blue" : "destructive"}
-    disabled={!job.isOpen || applied }
-    >
+    <Drawer open={applied ? false : undefined}>
+      <DrawerTrigger asChild>
+        <Button
+          size="lg"
+          variant={job?.isOpen && !applied ? "blue" : "destructive"}
+          disabled={!job?.isOpen || applied}
+        >
+          {job?.isOpen ? (applied ? "Applied" : "Apply") : "Hiring Closed"}
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>
+            Apply for {job?.title} at {job?.company?.name}
+          </DrawerTitle>
+          <DrawerDescription>Please Fill the form below.</DrawerDescription>
+        </DrawerHeader>
 
-        {job.isOpen ? (applied ? "Applied " : "Apply") : "Hiring Closed" }
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-4 p-4 pb-0"
+        >
+          <Input
+            type="number"
+            placeholder="Years of Experience"
+            className="flex-1"
+            {...register("experience", {
+              valueAsNumber: true,
+            })}
+          />
+          {errors.experience && (
+            <p className="text-red-500">{errors.experience.message}</p>
+          )}
 
-    </Button>
+          <Input
+            type="text"
+            placeholder="Skills (Comma Separated)"
+            className="flex-1"
+            {...register("skills")}
+          />
+          {errors.skills && (
+            <p className="text-red-500">{errors.skills.message}</p>
+          )}
 
-    </DrawerTrigger > 
-  <DrawerContent>
-    <DrawerHeader>
-      <DrawerTitle>
-        Apply for {job?.title} at {job?.company?.name}
-        </DrawerTitle>
-      <DrawerDescription>Please fill the form below</DrawerDescription>
-    </DrawerHeader>
+          <Controller
+            name="education"
+            control={control}
+            render={({ field }) => (
+              <RadioGroup onValueChange={field.onChange} {...field}>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="Intermediate" id="intermediate" />
+                  <Label htmlFor="intermediate">Intermediate</Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="Graduate" id="graduate" />
+                  <Label htmlFor="graduate">Graduate</Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="Post Graduate" id="post-graduate" />
+                  <Label htmlFor="post-graduate">Post Graduate</Label>
+                </div>
+              </RadioGroup>
+            )}
+          />
+          {errors.education && (
+            <p className="text-red-500">{errors.education.message}</p>
+          )}
 
-    <form className='flex flex-col gap-4 p-4 pb-0'>
+          <Input
+            type="file"
+            accept=".pdf, .doc, .docx"
+            className="flex-1 file:text-gray-500"
+            {...register("resume")}
+          />
+          {errors.resume && (
+            <p className="text-red-500">{errors.resume.message}</p>
+          )}
+          {errorApply?.message && (
+            <p className="text-red-500">{errorApply?.message}</p>
+          )}
+          {loadingApply && <BarLoader width={"100%"} color="#36d7b7" />}
+          <Button type="submit" variant="blue" size="lg">
+            Apply
+          </Button>
+        </form>
 
-        <Input 
-        type="number"
-        placeholder ="years of experience"
-        classNamw="flex-1 " 
-        />
+        <DrawerFooter>
+          <DrawerClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+};
 
-<RadioGroup defaultValue="option-one">
-  <div className="flex items-center space-x-2">
-    <RadioGroupItem value="Intermediate" id="intermediate" />
-    <Label htmlFor="intermediate">Intermediate</Label>
-  </div>
-  <div className="flex items-center space-x-2">
-    <RadioGroupItem value="Graduate" id="graduate" />
-    <Label htmlFor="option-two">Graduate</Label>
-  </div>
-  <div className="flex items-center space-x-2">
-    <RadioGroupItem value="post-graduate" id="post-graduate" />
-    <Label htmlFor="option-two"> Post Graduate</Label>
-  </div>
-</RadioGroup>
 
-<Input 
-        type="file"
-        placeholder =".pdf,.doc, .docx"
-        classNamw="flex-1 file:text-gray-500"
-        />
-
-        
-<Button type="submit" variant="blue" size='lg'
->Apply</Button>
-       
-    </form>
-
-    <DrawerFooter>
-      <DrawerClose asChild>
-        <Button variant="outline">Cancel</Button>
-      </DrawerClose>
-    </DrawerFooter>
-  </DrawerContent>
-</Drawer>
-  )
-}
-
-export default ApplyJobDrawer
+export default ApplyJobDrawer;
